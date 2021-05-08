fe7bd3ce3dafb3b8ad607b410a866caaf75b26d3
cleytonRR <cleytonrodrigues69@live.com>
2021-05-08 12:13:04 -0300

    Added plugin to database

--- api/plugins/repositories/admin/src/containers/HomePage/index.js
+++ api/plugins/repositories/admin/src/containers/HomePage/index.js
@@ -33,7 +33,7 @@ const HomePage = () => {
 
   useEffect(() => {
     axios
-      .get("https://api.github.com/users/React-aancado/repos")
+      .get("https://api.github.com/users/React-avancado/repos")
       .then((res) => setRows(res.data))
       .catch((e) =>
         strapi.notification.error(`Ops... github API limit exceeded. ${e}`)
