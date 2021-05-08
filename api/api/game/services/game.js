d78c09d6c17fba91b8145bb3ba4daaa3d233fb73
cleytonRR <cleytonrodrigues69@live.com>
2021-05-08 15:27:31 -0300

    Added method to create many to many datas

--- api/api/game/services/game.js
+++ api/api/game/services/game.js
@@ -6,6 +6,22 @@
  */
 
 const axios = require("axios");
+const slugify = require("slugify");
+
+async function getByName(name, entityName) {
+  const item = await strapi.services[entityName].find({ name });
+  return item.length ? item[0] : null;
+}
+
+async function create(name, entityName) {
+  const item = await getByName(name, entityName);
+  if (!item) {
+    return await strapi.services[entityName].create({
+      name,
+      slug: slugify(name, { lower: true }),
+    });
+  }
+}
 
 async function getGameInfo(slug) {
   const jsdom = require("jsdom");
@@ -22,6 +38,36 @@ async function getGameInfo(slug) {
   };
 }
 
+async function createManyToManyData(products) {
+  const developers = {};
+  const publishers = {};
+  const categories = {};
+  const platforms = {};
+
+  products.forEach((product) => {
+    const { developer, publisher, genres, supportedOperatingSystems } = product;
+
+    genres &&
+      genres.forEach((genre) => {
+        categories[genre] = true;
+      });
+    supportedOperatingSystems &&
+      supportedOperatingSystems.forEach((system) => {
+        platforms[system] = true;
+      });
+
+    developers[developer] = true;
+    publishers[publisher] = true;
+  });
+
+  return Promise.all([
+    ...Object.keys(developers).map((name) => create(name, "developer")),
+    ...Object.keys(publishers).map((name) => create(name, "publisher")),
+    ...Object.keys(categories).map((name) => create(name, "category")),
+    ...Object.keys(platforms).map((name) => create(name, "platform")),
+  ]);
+}
+
 module.exports = {
   populate: async (params) => {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
@@ -29,6 +75,7 @@ module.exports = {
     const {
       data: { products },
     } = await axios.get(gogApiUrl);
-    console.log(await getGameInfo(products[0].slug));
+
+    await createManyToManyData([products[2], products[3]]);
   },
 };
