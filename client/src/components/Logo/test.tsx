0f783683fdcf911f58e7052f36c47337da3d23a9
cleytonRR <cleytonrodrigues69@live.com>
2021-05-09 18:23:16 -0300

    Added props to define width

--- client/src/components/Logo/test.tsx
+++ client/src/components/Logo/test.tsx
@@ -5,14 +5,30 @@ import Logo from './index'
 
 describe('<Logo />', () => {
   it('Should render a white label by default ', () => {
-    // Render o component 'render'
-    // Selecionar o elemento a ser testado 'screen' (queries) = getByLabel
-    //Expect - assertion - asserção - comparação - análise( espero que renderize a logo branca)
-    // Enzime faz shallow - ou seja - não renderiza de fato
-
     renderWithTheme(<Logo />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: '#FAFAFA'
+    })
+  })
+
+  it('Should render a black when color is passed', () => {
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
+  })
+
+  it('Should render a normal logo when size is default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it('Should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
     })
   })
 })
