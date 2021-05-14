d368ebb0cffc31e8d63fd94c06032706a3ab1987
cleytonRR <cleytonrodrigues69@live.com>
2021-05-14 01:16:31 -0300

    Ensure render medium button by default

--- client/src/components/Button/test.tsx
+++ client/src/components/Button/test.tsx
@@ -1,12 +1,16 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/helpers'
 
 import Button from './index'
 
 describe('<Button />', () => {
-  it('Should render the heading', () => {
-    const { container } = render(<Button />)
-    expect(screen.getByRole('heading', { name: /Button/i })).toBeInTheDocument()
+  it('Should render the medium button by default', () => {
+    renderWithTheme(<Button>Buy now</Button>)
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      height: '4rem',
+      padding: '0.8rem 3.2rem',
+      'font-size': '1.4rem'
+    })
   })
 })
