c3bb8ae1c2005c4633594e2e283a5fc4a4ee6a00
cleytonRR <cleytonrodrigues69@live.com>
2021-05-14 23:22:48 -0300

    Added icon to button

--- client/src/components/Button/test.tsx
+++ client/src/components/Button/test.tsx
@@ -1,17 +1,22 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/helpers'
 
+// Icons
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
+
 import Button from './index'
 
 describe('<Button />', () => {
   it('Should render the medium button by default', () => {
-    renderWithTheme(<Button>Buy now</Button>)
+    const { container } = renderWithTheme(<Button>Buy now</Button>)
 
     expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
       height: '4rem',
       padding: '0.8rem 3.2rem',
       'font-size': '1.4rem'
     })
+
+    expect(container.firstChild).toMatchSnapshot()
   })
   it('Should render the small size', () => {
     renderWithTheme(<Button size="small">Buy now</Button>)
@@ -39,5 +44,14 @@ describe('<Button />', () => {
     expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
       width: '100%'
     })
+  })
+
+  it('Should render an icon version', () => {
+    renderWithTheme(
+      <Button icon={<AddShoppingCart data-testid="icon" />}>Buy now</Button>
+    )
+
+    expect(screen.getByText(/buy now/i)).toBeInTheDocument()
+    expect(screen.getByTestId('icon')).toBeInTheDocument()
   })
 })
