238878e5f5dd585e78d5c0638382e2a8ce2ccbde
cleytonRR <cleytonrodrigues69@live.com>
2021-05-15 00:26:01 -0300

    Created mobile menu and ensure icons in menu

---
+++ client/src/components/Menu/test.tsx
@@ -0,0 +1,15 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/helpers'
+
+import Menu from './index'
+
+describe('<Menu />', () => {
+  it('Should render the heading', () => {
+    renderWithTheme(<Menu />)
+
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/logo won games/i)).toBeInTheDocument()
+  })
+})
