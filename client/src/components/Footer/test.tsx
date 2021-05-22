8bcf474ae4a2c306e455dda6e1362a39d51d2048
cleytonRR <cleytonrodrigues69@live.com>
2021-05-22 20:08:16 -0300

    Created footer

---
+++ client/src/components/Footer/test.tsx
@@ -0,0 +1,28 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/helpers'
+
+import Footer from './index'
+
+describe('<Footer />', () => {
+  it('Should render 4 columns topics', () => {
+    // Contact
+    // follow us
+    // links
+    // location
+
+    renderWithTheme(<Footer />)
+    expect(
+      screen.getByRole('heading', { name: /contact/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /follow us/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /links/i })).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Location/i })
+    ).toBeInTheDocument()
+  })
+})
