9c85e938e07f018fefd371dfc2de25d55888eeb9
cleytonRR <cleytonrodrigues69@live.com>
2021-05-22 19:12:21 -0300

    Added custom color in border

--- client/src/components/Heading/test.tsx
+++ client/src/components/Heading/test.tsx
@@ -22,7 +22,7 @@ describe('<Heading />', () => {
   it('Should render a heading with a line to the left side', () => {
     renderWithTheme(<Heading lineLeft>Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-      'border-left': '0.7rem solid #3CD3C1'
+      'border-left': '0.7rem solid #F231A5'
     })
   })
 
@@ -57,5 +57,33 @@ describe('<Heading />', () => {
         modifier: '::after'
       }
     )
+  })
+
+  it('Should render a heading with a primary line color', () => {
+    renderWithTheme(
+      <Heading lineBottom lineLeft lineColor="primary">
+        Won Games
+      </Heading>
+    )
+
+    const heading = screen.getByRole('heading', { name: /won games/i })
+    expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #F231A5' })
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #F231A5', {
+      modifier: '::after'
+    })
+  })
+
+  it('Should render a heading with a secondary line color', () => {
+    renderWithTheme(
+      <Heading lineBottom lineLeft lineColor="secondary">
+        Won Games
+      </Heading>
+    )
+
+    const heading = screen.getByRole('heading', { name: /won games/i })
+    expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #3CD3C1' })
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #3CD3C1', {
+      modifier: '::after'
+    })
   })
 })
