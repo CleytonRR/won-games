96dab02e5c9c6813bdecd535136a98663672823f
cleytonRR <cleytonrodrigues69@live.com>
2021-05-23 00:57:53 -0300

    Created component and define styles to change color

---
+++ client/src/components/Ribbon/test.tsx
@@ -0,0 +1,27 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/helpers'
+
+import Ribbon from './index'
+
+describe('<Ribbon />', () => {
+  it('Should render the heading', () => {
+    renderWithTheme(<Ribbon>Best Seller</Ribbon>)
+
+    expect(screen.getByText(/Best Seller/i)).toBeInTheDocument()
+  })
+
+  it('Should render with the primary color', () => {
+    renderWithTheme(<Ribbon>Best Seller</Ribbon>)
+
+    expect(screen.getByText(/Best Seller/i)).toHaveStyle({
+      backgroundColor: '#F231A5'
+    })
+  })
+  it('Should render with the primary color', () => {
+    renderWithTheme(<Ribbon color="secondary">Best Seller</Ribbon>)
+
+    expect(screen.getByText(/Best Seller/i)).toHaveStyle({
+      backgroundColor: '#3CD3C1'
+    })
+  })
+})
