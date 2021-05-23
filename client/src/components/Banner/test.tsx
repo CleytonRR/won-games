cd3c323b47c6dc362aa4220341df7fcee1fa82af
cleytonRR <cleytonrodrigues69@live.com>
2021-05-23 00:28:06 -0300

    Created styles to carousel

--- client/src/components/Banner/test.tsx
+++ client/src/components/Banner/test.tsx
@@ -13,7 +13,7 @@ const props = {
 
 describe('<Banner />', () => {
   it('Should render the heading', () => {
-    renderWithTheme(<Banner {...props} />)
+    const { container } = renderWithTheme(<Banner {...props} />)
     expect(
       screen.getByRole('heading', { name: /Defy death/i })
     ).toBeInTheDocument()
@@ -23,5 +23,7 @@ describe('<Banner />', () => {
     ).toBeInTheDocument()
 
     expect(screen.getByRole('img', { name: /Defy death/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
