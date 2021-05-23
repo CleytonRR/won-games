d7419272d350917e316fe3b13bad23d117118c25
cleytonRR <cleytonrodrigues69@live.com>
2021-05-23 01:16:04 -0300

    Created Ribbon and added types

--- client/src/components/Ribbon/test.tsx
+++ client/src/components/Ribbon/test.tsx
@@ -17,11 +17,30 @@ describe('<Ribbon />', () => {
       backgroundColor: '#F231A5'
     })
   })
-  it('Should render with the primary color', () => {
+
+  it('Should render with the secondary color', () => {
     renderWithTheme(<Ribbon color="secondary">Best Seller</Ribbon>)
 
     expect(screen.getByText(/Best Seller/i)).toHaveStyle({
       backgroundColor: '#3CD3C1'
+    })
+  })
+
+  it('Should render with the size as default', () => {
+    renderWithTheme(<Ribbon>Best Seller</Ribbon>)
+
+    expect(screen.getByText(/Best Seller/i)).toHaveStyle({
+      height: '3.6rem',
+      fontSize: '1.4rem'
+    })
+  })
+
+  it('Should render with the small size', () => {
+    renderWithTheme(<Ribbon size="small">Best Seller</Ribbon>)
+
+    expect(screen.getByText(/Best Seller/i)).toHaveStyle({
+      height: '2.6rem',
+      fontSize: '1.2rem'
     })
   })
 })
