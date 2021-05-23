a47859cd9aef39a35d0739c5ac49f95e086a8dd1
cleytonRR <cleytonrodrigues69@live.com>
2021-05-23 00:02:51 -0300

    Refactor test

--- client/src/components/Banner/test.tsx
+++ client/src/components/Banner/test.tsx
@@ -3,17 +3,17 @@ import { renderWithTheme } from 'utils/helpers'
 
 import Banner from './index'
 
+const props = {
+  img: 'https://souce.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
 describe('<Banner />', () => {
   it('Should render the heading', () => {
-    renderWithTheme(
-      <Banner
-        img="https://souce.unsplash.com/user/willianjusten/1042x580"
-        title="Defy death"
-        subtitle="<p>Play the new <strong>CrashLands</strong> season</p>"
-        buttonLabel="Buy now"
-        buttonLink="/games/defy-death"
-      />
-    )
+    renderWithTheme(<Banner {...props} />)
     expect(
       screen.getByRole('heading', { name: /Defy death/i })
     ).toBeInTheDocument()
@@ -22,6 +22,6 @@ describe('<Banner />', () => {
       screen.getByRole('heading', { name: /Play the new CrashLands season/i })
     ).toBeInTheDocument()
 
-    expect(screen.getByLabelText(/Defy death/i)).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /Defy death/i })).toBeInTheDocument()
   })
 })
