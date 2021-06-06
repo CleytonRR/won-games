e7856543eef54d8d4bb21556ea685d092c0ab32a
cleytonRR <cleytonrodrigues69@live.com>
2021-06-05 23:52:10 -0300

    Added styles

--- client/src/components/GameCard/styles.ts
+++ client/src/components/GameCard/styles.ts
@@ -1,17 +1,75 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Wrapper = styled.main``
 
 export const ImageBox = styled.div``
 
-export const Info = styled.div``
+export const Content = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    position: relative;
+    height: 100%;
+    margin: ${theme.spacings.xsmall};
+  `}
+`
 
-export const Title = styled.h3``
+export const Info = styled.div`
+  max-width: calc(100% - 2.5rem);
+`
 
-export const Developer = styled.h4``
+export const Title = styled.h3`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.medium};
+    line-height: ${theme.font.sizes.medium};
+    font-weight: ${theme.font.bold};
+    color: ${theme.colors.black};
+  `}
+`
 
-export const FavButton = styled.div``
+export const Developer = styled.h4`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.small};
+    font-weight: ${theme.font.bold};
+    color: ${theme.colors.gray};
+  `}
+`
 
-export const BuyBox = styled.div``
+export const FavButton = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.colors.primary};
+    position: absolute;
+    right: 0;
+    top: -0.5rem;
+    cursor: pointer;
 
-export const Price = styled.div``
+    svg {
+      width: 2.5rem;
+    }
+  `}
+`
+
+export const BuyBox = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    margin-top: ${theme.spacings.xxsmall};
+  `}
+`
+
+export const Price = styled.div`
+  ${({ theme }) => css`
+    display: inline-flex;
+    font-weight: ${theme.font.bold};
+    height: 3rem;
+    align-items: center;
+
+    color: ${theme.colors.white};
+    padding: 0 ${theme.spacings.xxsmall};
+    background-color: ${theme.colors.secondary};
+    border-right: ${theme.border.radius};
+    margin-right: calc(${theme.spacings.xxsmall} / 2);
+  `}
+`
