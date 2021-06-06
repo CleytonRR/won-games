2642701eb4d01502e7dfe46e65a671e2e86c97f0
cleytonRR <cleytonrodrigues69@live.com>
2021-06-05 21:26:39 -0300

    Created game card

---
+++ client/src/components/GameCard/test.tsx
@@ -0,0 +1,29 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/helpers'
+
+import GameCard from './index'
+
+const props = {
+  title: 'Population Zero',
+  developer: 'Rockstar Games',
+  img: 'https://source.unsplash.com/user/willianjusten/300x140',
+  price: 'R$ 235,00'
+}
+
+describe('<GameCard />', () => {
+  it('Should render the heading', () => {
+    renderWithTheme(<GameCard {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: props.title })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: props.developer })
+    ).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      props.img
+    )
+    expect(screen.getByLabelText(/add to wishlist/i)).toBeInTheDocument()
+  })
+})
