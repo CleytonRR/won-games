16c83d6c872f6dd1367c0c76ee3a1318574f5c07
cleytonRR <cleytonrodrigues69@live.com>
2021-06-12 23:28:49 -0300

    Ensure render dots in slider

--- client/src/components/BannerSlider/test.tsx
+++ client/src/components/BannerSlider/test.tsx
@@ -43,4 +43,10 @@ describe('<BannerSlider />', () => {
       screen.getByRole('heading', { name: /defy death 2/i, hidden: true })
     ).toBeInTheDocument()
   })
+
+  it('Should render with the dots', () => {
+    const { container } = renderWithTheme(<BannerSlider items={items} />)
+
+    expect(container.querySelector('.slick-dots')).toBeInTheDocument()
+  })
 })
