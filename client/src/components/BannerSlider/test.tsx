d0f19fe134bcf3cb7aedbd6a65df1a772b1d4c89
cleytonRR <cleytonrodrigues69@live.com>
2021-06-12 23:22:03 -0300

    Created banner slider

---
+++ client/src/components/BannerSlider/test.tsx
@@ -0,0 +1,31 @@
+import 'match-media-mock'
+
+import { renderWithTheme } from 'utils/helpers'
+
+import BannerSlider from './index'
+
+const items = [
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death',
+    ribbon: 'Bestselling'
+  },
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death 2',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  }
+]
+
+describe('<BannerSlider />', () => {
+  it('Should render vertical slider', () => {
+    const { container } = renderWithTheme(<BannerSlider items={items} />)
+
+    expect(container.querySelector('.slick-vertical')).toBeInTheDocument()
+  })
+})
